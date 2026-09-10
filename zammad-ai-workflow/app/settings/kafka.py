a93f57b2eaaeee818d58a96b6933b14cf430d50b
Paul Ilitz <paul.ilitz@muenchen.de>
2026-09-10 10:09:32 +0200

    fix(kafka): fix max worker type

--- zammad-ai-workflow/app/settings/kafka.py
+++ zammad-ai-workflow/app/settings/kafka.py
@@ -2,7 +2,7 @@
 
 from typing import Literal
 
-from pydantic import BaseModel, Field, FilePath, NonNegativeInt
+from pydantic import BaseModel, Field, FilePath, NonNegativeInt, PositiveInt
 
 
 class KafkaSettings(BaseModel):
@@ -57,7 +57,7 @@ class KafkaSettings(BaseModel):
         description="Settings related to processing of incoming events.",
         default_factory=lambda: EventProcessingSettings(),
     )
-    max_workers: NonNegativeInt = Field(
+    max_workers: PositiveInt = Field(
         description="Maximum number of concurrent workers for processing events.",
         default=5,
     )
